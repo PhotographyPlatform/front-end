3a22b43adcfc824c8cd04d2041f452bb5757599f
mohAttallah <mohmohmoud28@gmail.com>
2023-10-08 00:50:15 +0000

    fix the layout

--- src/components/Layout/SideNavBar/index.jsx
+++ src/components/Layout/SideNavBar/index.jsx
@@ -5,11 +5,29 @@ import "./style/profile.scss"
 import { FaHome, FaUser, FaSearch, FaSign } from 'react-icons/fa';
 import { IoMdAddCircle } from 'react-icons/io';
 import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
-//Link
+
+
 // ⚠  Attention !!! ⚠
-import { NavLink } from 'react-router-dom';
 
 // This page is for the NavBar and NavSideBar. The page has two parts: one for mid and large screens and another for phone screens.
+
+
+// I adhere to the rules in the comment to add a link
+
+// If you don't know how to handle the link , contact with Mohammad Attallah, don't try anything else here. This is not a perfect place to try something you don't know how it works. 
+
+
+
+//                      ⬆⬆⬆⬆⬆⬆
+// read the Comment Carefully 
+
+
+
+
+
+import { NavLink } from 'react-router-dom';
+
+
 import NavMenuList from './MenuList.jsx';
 import DrawerPhone from './DrawerPhone';
 import { useDispatch } from 'react-redux';
@@ -32,9 +50,11 @@ function SideNavBar(props) {
     return (
         <nav>
             <div className='phone-item'>
+
                 {/* This header is only for phones;
-                    no editing is required here
+                    no editing is required here !!
                 */}
+
                 <header className='phone-nav-header'>
                     <h1 className='phoneLogo'>LOGO </h1>
                     <DrawerPhone />
@@ -51,17 +71,9 @@ function SideNavBar(props) {
                 </div>
 
                 <div className='logo'>LOGO</div>
+
+                {/* You Can Edit the links fromh ere */}
                 <div className='Links'>
-
-                    {/* <div className='link-card hover-nav'>
-                        <FaHome />
-                        <Link href='/'>
-                            <span className='links-title' >
-                                Home
-                            </span>
-                        </Link>
-                    </div> */}
-
                     <NavLink to='/' className='link-card hover-nav'>
 
                         <FaHome />
@@ -91,51 +103,36 @@ function SideNavBar(props) {
                         <span className='links-title more-title mid-nav-menu  disable-hover'>
                             More
                         </span>
-
-                    </div>
-
-                    <div className='link-card hover-nav'>
-                        <FaSign />
-                        <span className='links-title' onClick={onOpen}>
-                            Log Out
-                        </span>
-                        <Modal isOpen={isOpen} onClose={onClose}>
-                            <ModalOverlay />
-                            <ModalContent>
-                                <ModalHeader>Modal Title</ModalHeader>
-                                <ModalBody>
-                                    Are You Sure To Logout?
-                                </ModalBody>
-                                <ModalFooter>
-                                    <Button colorScheme='red' mr={3} onClick={handleLogout}>
-                                        Logout
-                                    </Button>
-                                    <Button onClick={onClose}>Cancel</Button>
-                                </ModalFooter>
-                            </ModalContent>
-                        </Modal>
                     </div>
 
                     {/*
                      You can add a new URL here. The link appears in the  NavSideBar for mid and large screens, and at the bottom for phones
 
+                    
+                    Remember, if you don't know how this  one works, contact Mohammad Attallah. 
 
-                    can add url like this oen like this one 
+                    You  can add a URL like this one
+                    
                                    ⬇⬇⬇⬇⬇⬇
 
-                   <div className='link-card nav-menu-list'>
-                        <NavMenuList />
-                        <span className='links-title more-title'>
-                            More
+                     <NavLink to='/pathexample' 
+                     className='link-card hover-nav'>
+                        <Icon />
+                        <span className='links-title'>
+                            Link Title
                         </span>
-
-                    </div>
-                    
-                    
+                    </NavLink>
                     */}
 
 
+                    {/*Add the Link Here !!  */}
+
+
+
+
+
                     {/*This contains user data and a link to the profile */}
+
                     <NavLink to='/profile' className='link-card parent-profile'>
                         <img src="https://i.pinimg.com/564x/1e/71/f0/1e71f00386068f5735d579f0f94b9af7.jpg" alt="" />
                         <span className='links-title disable-hover'>
